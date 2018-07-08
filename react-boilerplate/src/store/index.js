4c6fbb62eed5c5d709e5a3683ecbf59b20aaeb50
cheulong <searcheulong@gmail.com>
2018-07-08 22:50:45 +0700

    fianl

--- react-boilerplate/src/store/index.js
+++ react-boilerplate/src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers';
 const initState={};
 const middleware=[thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store =createStore(rootReducer,initState,composeEnhancers( applyMiddleware(...middleware)));
+const store =createStore(rootReducer,initState, applyMiddleware(...middleware));
 export default store;
